8aab31a1d01f6a8af1cdd0371731539cfab33e19
Azema <manuel.hervo@gmail.com>
2025-01-19 20:28:52 +0100

    Le nom du fichier à imprimer contient le nom du chantier et la date

--- js/celiane.js
+++ js/celiane.js
@@ -371,7 +371,7 @@ $(async function() {
           WinPrint.document.write(document.getElementById('result').outerHTML + '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" media="screen,print" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">');
           WinPrint.document.close();
           WinPrint.addEventListener("beforeprint", (event) => {
-            WinPrint.document.title='Commande Celiane';
+            WinPrint.document.title = `Commande Celiane - ${chantier} - ${today.toLocaleString()}`;
           });
           WinPrint.focus();
           WinPrint.print();
