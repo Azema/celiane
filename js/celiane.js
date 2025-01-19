c9c507c970869fb8d632e9502de523af8178f313
Azema <manuel.hervo@gmail.com>
2025-01-19 20:26:00 +0100

    Modif du titre de la page d'impression, ce qui change le nom du fichier à sauvegarder

--- js/celiane.js
+++ js/celiane.js
@@ -370,6 +370,9 @@ $(async function() {
           var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
           WinPrint.document.write(document.getElementById('result').outerHTML + '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" media="screen,print" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">');
           WinPrint.document.close();
+          WinPrint.addEventListener("beforeprint", (event) => {
+            WinPrint.document.title='Commande Celiane';
+          });
           WinPrint.focus();
           WinPrint.print();
           // WinPrint.close();
